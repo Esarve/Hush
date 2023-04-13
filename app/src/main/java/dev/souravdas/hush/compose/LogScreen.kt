02bcb7429649136bec81fb20f7383ed7f0b99ce1
Sourav Das <esarve.srv@gmail.com>
2023-04-13 20:48:45 +0600

    updated data not showing

--- app/src/main/java/dev/souravdas/hush/compose/LogScreen.kt
+++ app/src/main/java/dev/souravdas/hush/compose/LogScreen.kt
@@ -1,15 +1,25 @@
 package dev.souravdas.hush.compose
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -18,8 +28,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.currentOrThrow
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
 import dev.souravdas.hush.arch.MainActivityVM
 import dev.souravdas.hush.models.AppLog
@@ -37,24 +45,20 @@ import dev.souravdas.hush.others.Utils
 @Composable
 fun AppLogList() {
     val viewModel: MainActivityVM = viewModel()
-    val navigator = LocalNavigator.currentOrThrow
-
     val logsState = viewModel.appLog.collectAsState(emptyList())
-    val logs by rememberSaveable { logsState }
+    val logs by remember { logsState }
 
     SideEffect {
         viewModel.getLog()
     }
 
-
     LazyColumn(
 
     ) {
-        items(logs) { log ->
+        items(logs, key = {it.id}) { log ->
             ItemView(log)
         }
     }
-
 
 }
 
